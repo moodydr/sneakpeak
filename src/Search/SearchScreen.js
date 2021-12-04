1b3e0a910fc246101091d4a3fe43e1702f16eb03
Mac Moody <draytonmoody@gmail.com>
2021-12-04 15:17:55 -0800

    fixed search for no results

--- src/Search/SearchScreen.js
+++ src/Search/SearchScreen.js
@@ -5,7 +5,7 @@ import {Link, useNavigate, useParams} from "react-router-dom";
 const SearchScreen = () =>{
     const params = useParams();
     const navigate = useNavigate();
-    const movieTitle = params.searchTerm || 'batman'
+    const movieTitle = params.searchTerm || 'batman';
     const [searchTerm, setSearchTerm] = useState(movieTitle)
     const [movies, setMovies] = useState([]);
     const findMovies = () =>
@@ -15,6 +15,11 @@ const SearchScreen = () =>{
             .then(res => res.json())
             .then(results => setMovies(results.Search))
     }
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            console.log(e.key);
+        }
+    };
     //use useEffect for functions when you first load
     useEffect(findMovies, []);
     return(
@@ -22,14 +27,15 @@ const SearchScreen = () =>{
             <div className="container">
             <h1>Search Screen</h1>
             <div class="input-group">
-                <input type="text"  className="" placeholder="Find a movie..." onChange={(e) =>
+                <input type="text"  className="" placeholder="Find a movie..." onKeyPress={(e) =>
+                handleKeyPress(e.key)} onChange={(e) =>
                     setSearchTerm(e.target.value)} />
                 <button onClick={findMovies} type="button" className="btn btn-primary">Search</button>
             </div>
             <div className="row mt-4">
 
-                {
-                    movies.map(movie =>
+                {movies
+                    ? movies.map(movie => (
                         // <li key={movie.imdbID}>
                         //     <Link to={`/sneakpeak/search/details/${movie.imdbID}`}>
                         //     <img alt="Poster" src={movie.Poster} height={125}/>
@@ -44,12 +50,12 @@ const SearchScreen = () =>{
                         // </li>
 
 
-                    <div className="col-sm-2">
-                        <Link to={`/sneakpeak/search/details/${movie.imdbID}`} >
-                            <div className="card" >
-                                <img src={movie.Poster} className="card-img-top" alt="..."/>
+                    <div className="col-md-3 col-4 col-lg-2">
+                        <Link className="no-underline" to={`/sneakpeak/search/details/${movie.imdbID}`} >
+                            <div className="card border-0" >
+                                <img src={movie.Poster} className="card-img-top"  alt="..."/>
                                 <div className="card-body">
-                                    <p className="card-text">{movie.Title}</p>
+                                    <p className="card-text fw-bold">{movie.Title}</p>
                                 </div>
                             </div>
 
@@ -61,7 +67,11 @@ const SearchScreen = () =>{
 
 
 
-                    )
+                    ))
+                    : <div className="container">
+                        <p>No results found.</p>
+
+                    </div>
                 }
             </div>
             {JSON.stringify(movies)}
